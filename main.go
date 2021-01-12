065523be6b1196fc498782010962a43812741536
Kyle L. Jensen <kljensen@gmail.com>
2021-01-11 20:53:59 -0500

    Finish working example

--- main.go
+++ main.go
@@ -4,72 +4,126 @@ import (
 	"context"
 	"io"
 	"os"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 )
 
 func main() {
 	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+
+	// Get a Docker API client
+	apiClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	// Download the Docker images we'll use
+	curlImage := "curlimages/curl"
+	httpBinImage := "mccutchen/go-httpbin"
+	images := []string{curlImage, httpBinImage}
+	for _, image := range images {
+		reader, err := apiClient.ImagePull(ctx, image, types.ImagePullOptions{})
+		if err != nil {
+			panic(err)
+		}
+		// Log the download
+		io.Copy(os.Stdout, reader)
+	}
+
+	// This is the "alias" for the httpbin container on
+	// our user-defined network.
+	httpBinDNSName := "httpbin"
+
+	// Config for httpbin
+	httpBinConfig := &container.Config{
+		Image: httpBinImage,
+		Tty:   false,
+	}
+
+	// Config for Curl
+	curlConfig := &container.Config{
+		Image: "curlimages/curl:latest",
+		Cmd:   []string{"curl", "http://" + httpBinDNSName + ":8080/get?foo=bar"},
+		Tty:   false,
+	}
+
+	// Create a user defined network. Each of our containers
+	// will be attached to this network.
+	networkID := "kljensen-golang-docker-link"
+	networkCreate := types.NetworkCreate{
+		Attachable:     true,
+		CheckDuplicate: true,
+	}
+	networkResult, err := apiClient.NetworkCreate(ctx, networkID, networkCreate)
 	if err != nil {
 		panic(err)
 	}
-	io.Copy(os.Stdout, reader)
 
-	postgresqlConfig := &container.Config{
-		Image: "alpine",
-		Cmd:   []string{"echo", "hello world"},
-		Tty:   false,
+	// Remove the network on panic or when `main` returns
+	defer apiClient.NetworkRemove(ctx, networkResult.ID)
+
+	// Create network config for the curl Docker container
+	curlNetworkConfig := &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{
+			"net": {
+				NetworkID: networkResult.ID,
+			},
+		},
 	}
 
-	alpineConfig := &container.Config{
-		Image: "alpine",
-		Cmd:   []string{"echo", "hello world"},
-		Tty:   false,
+	// Create Docker config for the httpbin Docker container.
+	// Notice how it is given an alias.
+	httpBinNetworkConfig := &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{
+			"net": {
+				Aliases:   []string{httpBinDNSName},
+				NetworkID: networkResult.ID,
+			},
+		},
 	}
 
-	// hostConfig := container.HostConfig{
-	// Links:
-	// }
+	// Create both containers
+	httpBinResp, err := apiClient.ContainerCreate(ctx, httpBinConfig, nil, httpBinNetworkConfig, nil, "")
+	if err != nil {
+		panic(err)
+	}
 
-	resps := make([]container.ContainerCreateCreatedBody, 0)
-	configs := []container.Config{*alpineConfig, *postgresqlConfig}
+	if err := apiClient.ContainerStart(ctx, httpBinResp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
 
-	for _, config := range configs {
-		resp, err := cli.ContainerCreate(ctx, &config, nil, nil, nil, "")
+	curlResp, err := apiClient.ContainerCreate(ctx, curlConfig, nil, curlNetworkConfig, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := apiClient.ContainerStart(ctx, curlResp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	// Wait for the curl request to end and its container to stop
+	statusCh, errCh := apiClient.ContainerWait(ctx, curlResp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
 		if err != nil {
 			panic(err)
 		}
-
-		if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-			panic(err)
-		}
-		resps = append(resps, resp)
+	case <-statusCh:
 	}
 
-	for _, resp := range resps {
-		statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-		select {
-		case err := <-errCh:
-			if err != nil {
-				panic(err)
-			}
-		case <-statusCh:
-		}
-
-		out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-		if err != nil {
-			panic(err)
-		}
-		stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	// Grab the curl logs and echo them to stdout
+	out, err := apiClient.ContainerLogs(ctx, curlResp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		panic(err)
 	}
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 
+	// Stop the httpbin container
+	timeout := 5 * time.Second
+	apiClient.ContainerStop(ctx, httpBinResp.ID, &timeout)
 }
